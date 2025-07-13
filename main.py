8125634ee207f6793894ce681d85a1abed7f88f8
BlackViking88 <kantad.svendsgaard@gmail.com>
2025-07-13 08:55:20 -0400

    final changes to main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
     word_count = words_in_document(text)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
